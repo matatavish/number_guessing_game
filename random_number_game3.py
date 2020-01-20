66d4125081dfb99901c93a3299ad6c1fc8ad5765
matatavish <58575477+matatavish@users.noreply.github.com>
2020-01-20 07:02:54 -0500

    Guess the number game.
    This is my first python program.  It works but, it could use some critiquing as I am new to programming and I really don't know what I am doing.

---
+++ random_number_game3.py
@@ -0,0 +1,41 @@
+import random
+
+play = True
+
+while play:
+    print(f'\nI am thinking of a number between 1 and 100. You have 5 guesses.')
+
+    secret_number = random.randint(1, 100)
+    guesses = 5
+    user_num = 0
+    play_again = ''
+
+    while guesses > 0:
+        print(f'Guesses remaining: {guesses}')
+        guesses -= 1
+        try:
+            user_num = int(input(f'\nEnter your guess: '))
+        except ValueError:
+            print('I need a number!')
+            guesses += 1
+            continue
+        if user_num == secret_number:
+            break
+        if user_num < secret_number:
+            print('Too low!')
+        if user_num > secret_number:
+            print('Too high!')
+
+    if user_num == secret_number:
+        print(f'\nHooray for you!  You guessed it!')
+    if user_num != secret_number:
+        print('You are out of guesses.')
+
+    while play_again not in {'y', 'n'}:
+        play_again = input(f'\nWould you like to try again? y/n')
+        play_again = play_again.lower()
+        if play_again not in {'y', 'n'}:
+            print(f'\nInvalid! y or n')
+        elif play_again == 'n':
+            print(f'\nHave a nice day!')
+            play = False
